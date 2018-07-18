94e9c9ec895133c17528d0d39859ed1df9d2e40a
kamelot43 <andrepud@mail.ru>
2018-07-18 10:58:06 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,8 @@ const rename = require(`gulp-rename`);
 const imagemin = require(`gulp-imagemin`);
 const rollup = require(`gulp-better-rollup`);
 const sourcemaps = require(`gulp-sourcemaps`);
+const mocha = require(`gulp-mocha`);
+const commonjs = require(`rollup-plugin-commonjs`);
 
 gulp.task(`style`, () => {
   return gulp.src(`sass/style.scss`).
@@ -44,6 +46,7 @@ gulp.task(`scripts`, () => {
     .pipe(gulp.dest(`build/js`));
 });
 
+
 gulp.task(`imagemin`, [`copy`], () => {
   return gulp.src(`build/img/**/*.{jpg,png,gif}`).
     pipe(imagemin([
@@ -52,6 +55,7 @@ gulp.task(`imagemin`, [`copy`], () => {
     ])).
     pipe(gulp.dest(`build/img`));
 });
+
 
 gulp.task(`copy-html`, () => {
   return gulp.src(`*.{html,ico}`).
@@ -102,5 +106,15 @@ gulp.task(`build`, [`assemble`], () => {
   gulp.start(`imagemin`);
 });
 
-gulp.task(`test`, () => {
+gulp.task(`test`, function () {
+  return gulp
+  .src([`js/**/*.test.js`])
+  .pipe(rollup({
+    plugins: [
+      commonjs() // Сообщает Rollup, что модули можно загружать из node_modules
+    ]}, `cjs`)) // Выходной формат тестов — `CommonJS` модуль
+  .pipe(gulp.dest(`build/test`))
+  .pipe(mocha({
+    reporter: `spec` // Вид в котором я хочу отображать результаты тестирования
+  }));
 });
